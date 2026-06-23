a199cfef2149c9c19f8617e97ef74c18b0f1a667
aaron <aaron@AARONMATHEW>
2026-06-23 14:40:49 +0530

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Calculator
+{
+    static void Main()
+    {
+        Console.WriteLine("Basic Calculator");
+
+        Console.Write("Enter first number: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine($"Addition: {Add(a, b)}");
+        Console.WriteLine($"Subtraction: {Subtract(a, b)}");
+        Console.WriteLine($"Multiplication: {Multiply(a, b)}");
+        Console.WriteLine($"Division: {Divide(a, b)}");
+
+
+        Console.ReadKey();
+    }
+
+
+    // Everyone will work below this line
+
+    public static double Add(double a, double b)
+    {
+        return 0;
+    }
+
+
+    public static double Subtract(double a, double b)
+    {
+        return 0;
+    }
+
+
+    public static double Multiply(double a, double b)
+    {
+        return 0;
+    }
+
+
+    public static double Divide(double a, double b)
+    {
+        return 0;
+    }
+
+}
